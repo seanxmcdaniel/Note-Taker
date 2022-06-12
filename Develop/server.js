9773ba43ef579623bfc4f8c35f4c5ddf2f316362
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-06-11 22:35:04 -0400

    Update port

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const uuid = require('./helpers/uuid');
 const app = express();
 
